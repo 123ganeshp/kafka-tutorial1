aaf6ccee7269c81dfc02b1bc80a62a0b6f27f9fd
Ganesh pawar <ganeshpawar@Ganeshs-MacBook-Air.local>
2025-12-01 22:27:34 +0530

    Initial commit

---
+++ src/main/java/com/kafka/SpringbootKafkaTutorial1Application.java
@@ -0,0 +1,13 @@
+package com.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaTutorial1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaTutorial1Application.class, args);
+	}
+
+}
